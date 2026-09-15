3312404c67491f8607ec976217551fcc2431fe18
luizhenriquepds1 <luizhenriquepds1@gmail.com>
2026-09-15 20:05:31 -0300

    atualizacao

--- 13_own_tools.py
+++ 13_own_tools.py
@@ -3,6 +3,9 @@ from agno.tools.tavily import TavilyTools
 from agno.models.groq import Groq
 #from agno.models.openai import OpenAIChat
 from dotenv import load_dotenv
+from agno.os import AgentOS
+from agno.db.sqlite import SqliteDb
+
 
 load_dotenv()
 
@@ -18,14 +21,27 @@ def celsius_to_fh(temperatura_celsius: float):
     """
     return (temperatura_celsius * 9/5) + 32
 
+
+db = SqliteDb(db_file="tmp/agent.db", session_table="agent_session")
+
 agent = Agent(
+    name="Agente do tempo",
     model=Groq(id="openai/gpt-oss-120b"),
     #model=OpenAIChat(id="gpt-4.1-mini"),
     tools=[
         TavilyTools(),
         celsius_to_fh,
     ],
-    debug_mode=True
+    db=db,
+    add_history_to_context=True,
+    num_history_runs=3,
+    #debug_mode=True
 )
 
-agent.print_response("Use suas ferramentas para pesquisar a temperatura de hoje em Recife e em Camaragibe-PE em Fahrenheit")
+agent_os = AgentOS(agents=[agent])
+app = agent_os.get_app()
+
+if __name__=="__main__":
+    agent_os.serve(app="13_own_tools:app", reload=True)
+
+#agent.print_response("Use suas ferramentas para pesquisar a temperatura de hoje em Recife e em Camaragibe-PE em Fahrenheit")
---
+++ 21_pdf_agent.py
@@ -0,0 +1,36 @@
+from agno.agent import Agent
+from agno.models.groq import Groq
+#from agno.models.openai import OpenAIChat
+from dotenv import load_dotenv
+from agno.os import AgentOS
+from agno.db.sqlite import SqliteDb
+from agno.knowledge.knowledge import Knowledge
+from agno.vectordb.chroma import ChromaDb
+from agno.knowledge.embedder.fastembed import FastEmbedEmbedder
+
+load_dotenv()
+
+vector_db = ChromaDb(
+    collection="pdf_agent", 
+    path="tmp/chromadb", 
+    persistent_client=True, 
+    embedder=FastEmbedEmbedder(),
+)
+
+knowledge = Knowledge(vector_db=vector_db, max_results=3)
+knowledge.insert(path="ENGENHARIA+DE+PROMPT.pdf")
+
+agent = Agent(
+    name="Agente de PDF",
+    model=Groq(id="openai/gpt-oss-120b"),
+    knowledge=knowledge,
+    search_knowledge=True,
+    add_history_to_context=True,
+    num_history_runs=4,
+)
+
+agent_os = AgentOS(agents=[agent])
+app = agent_os.get_app()
+
+if __name__=="__main__":
+    agent_os.serve(app="21_pdf_agent:app", reload=True)
